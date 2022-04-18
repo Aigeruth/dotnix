49afd0913d9595a262a12243f13ae9368e778b25
Gabor Nagy <mail@aigeruth.hu>
2022-04-18 15:31:00 +0100

    Add coreutils

--- modules/terminal.nix
+++ modules/terminal.nix
@@ -1,6 +1,13 @@
 { pkgs, ... }:
 
 {
-  home.packages =
-    [ pkgs.bat pkgs.exa pkgs.fd pkgs.ripgrep pkgs.vgrep pkgs.wget ];
+  home.packages = [
+    pkgs.bat
+    pkgs.coreutils
+    pkgs.exa
+    pkgs.fd
+    pkgs.ripgrep
+    pkgs.vgrep
+    pkgs.wget
+  ];
 }
