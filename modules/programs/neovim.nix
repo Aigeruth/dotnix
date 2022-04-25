6defb7a6e7f6692bd65b9c86ef7bf20046d24c0b
Gabor Nagy <mail@aigeruth.hu>
2022-04-25 14:01:18 +0100

    Configure ale for neovim

--- modules/programs/neovim.nix
+++ modules/programs/neovim.nix
@@ -1,14 +1,15 @@
 { pkgs, ... }:
 
 {
+  home.packages = [ pkgs.unstable.rnix-lsp pkgs.unstable.nixfmt ];
+
   programs.neovim = {
     enable = true;
 
     plugins = with pkgs.unstable.vimPlugins; [
+      ale
       dracula-vim
-      (nvim-treesitter.withPlugins (plugins: [
-        plugins.tree-sitter-nix
-      ]))
+      (nvim-treesitter.withPlugins (plugins: [ plugins.tree-sitter-nix ]))
     ];
 
     vimAlias = true;
@@ -20,6 +21,15 @@
     package = pkgs.unstable.neovim-unwrapped;
     extraConfig = ''
       colorscheme dracula
+
+      let g:ale_completion_enabled = 1
+      let g:ale_fix_on_save = 1
+      let g:ale_fixers = {
+      \  '*': ['remove_trailing_lines', 'trim_whitespace'],
+      \  'javascript': ['eslint', 'prettier'],
+      \  'nix': ['nixfmt'],
+      \  'python': ['black']
+      \}
 
       lua << EOF
         require'nvim-treesitter.configs'.setup {
