1774b2fafab827055ce9cfa9f100a2703eb5264e
Gabor Nagy <mail@aigeruth.hu>
2022-04-27 11:03:55 +0100

    Add home-manager for Jill

--- computers/Jill.nix
+++ computers/Jill.nix
@@ -12,10 +12,14 @@
         ../modules/terminal.nix
         ../modules/tools.nix
         ../modules/programs/fish.nix
+        ../modules/programs/home-manager.nix
         ../modules/programs/neovim.nix
+        ../modules/programs/emacs.nix
       ];
+
+      home.stateVersion = stateVersion;
     };
     # These are passed down to all home-manager modules
-    extraSpecialArgs = { inherit system pkgs stateVersion; };
+    extraSpecialArgs = { inherit system pkgs; };
   };
 }
