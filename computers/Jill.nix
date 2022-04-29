195fbe56e0bd5bd4c3a5848c25a89805edace7c3
Gabor Nagy <mail@aigeruth.hu>
2022-04-29 20:07:02 +0100

    Add starship.rs on Jill

--- computers/Jill.nix
+++ computers/Jill.nix
@@ -11,10 +11,10 @@
         ../modules/home-manager/development.nix
         ../modules/home-manager/terminal.nix
         ../modules/home-manager/tools.nix
-        ../modules/home-manager/programs/emacs.nix
         ../modules/home-manager/programs/fish.nix
         ../modules/home-manager/programs/home-manager.nix
         ../modules/home-manager/programs/neovim.nix
+        ../modules/home-manager/programs/starship.nix
       ];
 
       home.stateVersion = stateVersion;
