ff9f8835ca3e388aa54b4a9c8184ce584b1dddcd
Gabor Nagy <mail@aigeruth.hu>
2022-04-30 10:23:28 +0100

    Add Authy Desktop on Intel macOS

--- modules/darwin/brew/utils.nix
+++ modules/darwin/brew/utils.nix
@@ -1,9 +1,12 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
-  homebrew.masApps = {
-    "1Password 7 - Password Manager" = 1333542190;
-    "Tailscale" = 1475387142;
-    "Yubico Authenticator" = 1497506650;
+  homebrew = {
+    casks = (if pkgs.stdenv.hostPlatform.isAarch64 then [ ] else [ "authy" ]);
+    masApps = {
+      "1Password 7 - Password Manager" = 1333542190;
+      "Tailscale" = 1475387142;
+      "Yubico Authenticator" = 1497506650;
+    };
   };
 }
