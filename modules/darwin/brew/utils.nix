be68754cae3afee30577cf419156fb04c3a7cbbf
Gabor Nagy <mail@aigeruth.hu>
2022-05-11 20:39:43 +0100

    Upgrade to 1Password 8
    1Password 8 seems to work better with the 1Password for Safari plugin.

--- modules/darwin/brew/utils.nix
+++ modules/darwin/brew/utils.nix
@@ -2,9 +2,14 @@
 
 {
   homebrew = {
-    casks = if pkgs.stdenv.hostPlatform.isAarch64 then [ ] else [ "authy" ];
+    casks = if pkgs.stdenv.hostPlatform.isAarch64 then
+      [ ]
+    else [
+      "authy"
+      "1password" # 1Password 8 was released in May 2022
+    ];
     masApps = {
-      "1Password 7 - Password Manager" = 1333542190;
+      "1Password for Safari" = 1569813296;
       "Tailscale" = 1475387142;
       "Yubico Authenticator" = 1497506650;
     };
