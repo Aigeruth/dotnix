05680ed1a9d3a1331988034e47d676fc8d6f2539
Gabor Nagy <mail@aigeruth.hu>
2022-05-12 19:24:40 +0100

    Install 1Password 8 on all architectures

--- modules/darwin/brew/utils.nix
+++ modules/darwin/brew/utils.nix
@@ -2,12 +2,9 @@
 
 {
   homebrew = {
-    casks = if pkgs.stdenv.hostPlatform.isAarch64 then
-      [ ]
-    else [
-      "authy"
+    casks = [
       "1password" # 1Password 8 was released in May 2022
-    ];
+    ] ++ (if pkgs.stdenv.hostPlatform.isAarch64 then [ ] else [ "authy" ]);
     masApps = {
       "1Password for Safari" = 1569813296;
       "Tailscale" = 1475387142;
