d6d98d0e94aae5e7e10c32634feb37fabca5c3e8
Gabor Nagy <mail@aigeruth.hu>
2022-07-05 07:52:22 +0100

    Add Vale for work

--- computers/work.nix
+++ computers/work.nix
@@ -15,6 +15,7 @@
         ../modules/home-manager/programs/home-manager.nix
         ../modules/home-manager/programs/neovim.nix
         ../modules/home-manager/programs/starship.nix
+        ../modules/home-manager/programs/vale.nix
       ];
 
       home.stateVersion = stateVersion;
