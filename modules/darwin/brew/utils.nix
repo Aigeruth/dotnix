0fddd6fc7f167b68c3677fb51b3e7212d4bf4d0d
Gabor Nagy <mail@aigeruth.hu>
2022-09-06 19:24:29 +0100

    Remove 1Password for Safari

--- modules/darwin/brew/utils.nix
+++ modules/darwin/brew/utils.nix
@@ -6,7 +6,6 @@
       "1password" # 1Password 8 was released in May 2022
     ] ++ (if pkgs.stdenv.hostPlatform.isAarch64 then [ ] else [ "authy" ]);
     masApps = {
-      "1Password for Safari" = 1569813296;
       "Tailscale" = 1475387142;
       "Yubico Authenticator" = 1497506650;
     };
