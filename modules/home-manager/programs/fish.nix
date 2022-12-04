d55e5d07896de7a563fd603f39efc8fcd9740eb9
Gabor Nagy <mail@aigeruth.hu>
2022-12-04 11:51:57 +0000

    Add Visual Studio Code integration to fish

--- modules/home-manager/programs/fish.nix
+++ modules/home-manager/programs/fish.nix
@@ -27,6 +27,10 @@
         set -U fish_greeting
       '';
 
+      interactiveShellInit = ''
+        string match -q "$TERM_PROGRAM" "vscode" && source (code --locate-shell-integration-path fish)
+      '';
+
       shellAliases = {
         ia = ''open -a "/Applications/iA Writer.app" '';
         ls = "exa";
