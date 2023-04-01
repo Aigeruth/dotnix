6ac5bec39967be9e93b0185c5fde48c23376c1b4
Gabor Nagy <mail@aigeruth.hu>
2023-04-01 11:21:43 +0100

    Upgrade to Affinity Photo 2

--- modules/darwin/brew/photography.nix
+++ modules/darwin/brew/photography.nix
@@ -4,7 +4,7 @@ _:
   homebrew = {
     casks = [ "darktable" "rawtherapee" ];
     masApps = {
-      "Affinity Photo" = 824183456;
+      "Affinity Photo 2" = 1616822987;
       "Pixelmator Pro" = 1289583905;
       "RAW Power" = 1157116444;
     };
